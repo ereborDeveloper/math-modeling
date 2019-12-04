6d1b169141f4f23adcc0e752e1a5b073879218f7
ereborDeveloper <zhekadreamteam11@gmail.com>
2019-12-04 17:11:01 +0300

    Initial commit

---
+++ src/main/java/modeling/mathmodeling/controller/Controller.java
@@ -0,0 +1,12 @@
+package modeling.mathmodeling.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello!";
+    }
+}
